d27172ad47529d71aa74d80616320076b9ed1b53
Olgakim111 <kimolga@yahoo.com>
2018-06-12 16:12:52 -0500

    Area Square

--- sandbox/src/main/java/FirstProgram.java
+++ sandbox/src/main/java/FirstProgram.java
@@ -1,7 +1,25 @@
 public class FirstProgram {
     public static void main(String[] args) {
-        System.out.println("First Program");
+        hello("User!");
+        double len=5;
+        System.out.println("Area of Square with side "+ len +" = " +area(len));
+        double a=3;
+        double b=4;
+        System.out.println("Area of Rectangle with sides " + a + " and " + b + " = " +area(a,b));
        
+    }
+
+    public static void hello(String somebody){
+        System.out.println("Hello, "+somebody);
+    }
+
+    public static double area(double l){
+       return l*l;
+
+    }
+
+    public static double area(double a, double b){
+        return a*b;
     }
 
 }
